12577fcdd1e099cbef7a32ae9ec7788d4c6a0fef
Franklinjapa <140675725+Franklinjapa@users.noreply.github.com>
2024-01-07 19:15:44 -0300

    Add files via upload

---
+++ codigos.py
@@ -0,0 +1,10 @@
+print("Faturamento", faturamento
+print("Custo", custo)
+print ("Imposto", imposto)
+print ("Lucro", lucro)
+
+t1 = 10000 
+faturamento = t1 
+custo = 0.5 * faturamento 
+imposto = 0.1 * faturamento
+lucro = faturamento - custo - imposto
